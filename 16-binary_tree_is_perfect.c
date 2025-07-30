021f1ac8ff3a85bf3307210ffba38caaff6b1c36
Zeynab <zeyneb.gurbanva@gmail.com>
2025-07-30 21:16:09 +0000

    commit

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -51,7 +51,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 		return (0);
 
 	return (1 + binary_tree_size(tree->left) +
-	           binary_tree_size(tree->right));
+		binary_tree_size(tree->right));
 }
 
 /**
